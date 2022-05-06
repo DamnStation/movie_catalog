78552937dac68791dfa7cb90b85594049680666a
DamnStation <radarawow@gmail.com>
2022-05-06 14:28:42 +0300

    Added base layout for Home screen

---
+++ pages/components/Divider/Index.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <div className="flex flex-col w-full">
+      <div className="divider"></div>
+    </div>
+  );
+}
---
+++ pages/components/Favorites/index.tsx
@@ -0,0 +1,32 @@
+export default function index() {
+  return (
+    <>
+      <div className="grid  grid-flow-col m-1 auto-cols-max">
+        <div className="card w-45 bg-base-100 shadow-xl m-2">
+          <figure>
+            <img
+              src="https://api.lorem.space/image/shoes?w=400&h=225"
+              alt="Shoes"
+            />
+          </figure>
+        </div>
+        <div className="card w-45 bg-base-100 shadow-xl m-2">
+          <figure>
+            <img
+              src="https://api.lorem.space/image/shoes?w=400&h=225"
+              alt="Shoes"
+            />
+          </figure>
+        </div>
+        <div className="card w-45 bg-base-100 shadow-xl m-2">
+          <figure>
+            <img
+              src="https://api.lorem.space/image/shoes?w=400&h=225"
+              alt="Shoes"
+            />
+          </figure>
+        </div>
+      </div>
+    </>
+  );
+}
